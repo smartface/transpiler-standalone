398a85a70735b5321ef9c9c11dc44e0ed7072346
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2020-03-20 00:00:04 +0300

    Improve to run as npm module

--- packages/ws-builder/run.js
+++ packages/ws-builder/run.js
@@ -1,5 +1,13 @@
-const concurrently = require('concurrently');
+#!/usr/bin/env node
+
+var args = require('minimist')(process.argv.slice(2));
+const concurrently = require("concurrently");
 // env SMF_CIDE_WS_PATH=$PWD/$WS  concurrently -k 'sfLibraryManager' 'sfMarketplaceService serviceStart' 'transpiler --standalone'
+const standalone = args.standalone;
+// process.env.SMF_CIDE_WS_PATH = process.cwd();
+
+console.log("SMF_CIDE_WS_PATH : ", process.env.SMF_CIDE_WS_PATH);
+
 concurrently([
     'sfLibraryManager',
     'sfMarketplaceService serviceStart',
